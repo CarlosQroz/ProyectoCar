aa0e1500a820d92970dd84ca6eee21aff0e2ca71
carlos quiroz <carlos.quiroz@tecsup.edu.pe>
2023-07-09 17:28:44 -0500

    Api

---
+++ NetCore6APIMySQL.Data/Repositories/ICardRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetCore6APIMySQL.Model;
+
+namespace NetCore6APIMySQL.Data.Repositories
+{
+    public interface ICardRepository
+    {
+        Task<IEnumerable<Car>> GetallCars();
+        Task<Car> GetDetails(int id);
+        Task<bool> InsertCar(Car car);
+        Task<bool> UpdateCar(Car car);
+        Task<bool> DeleteCar(Car car);
+    }
+}
+
